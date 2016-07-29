eb4d2f3fd2dbfa96cf1c7ca5a3b3679bb1f7fc18
canfoo <178774618@qq.com>
2016-07-29 15:11:11 +0800

    react-demo

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+		'webpack/hot/only-dev-server',
+		'./index.jsx' // Your appʼs entry point
+	],
+	devtool: process.env.WEBPACK_DEVTOOL || 'source-map',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	devServer: {
+		contentBase: "./build",
+			noInfo: true, //  --no-info option
+			hot: true,
+			inline: true
+		},
+	plugins: [
+		new webpack.NoErrorsPlugin(),
+		new CopyWebpackPlugin([
+			{from: './index.html'}
+		]),
+	]
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+var loaders = require('./webpack.loaders');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'./index.jsx' // Your appʼs entry point
+	],
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
+	module: {
+		loaders: loaders
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{from: './index.html'}
+		]),
+	]
+};
